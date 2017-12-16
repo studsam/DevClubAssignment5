61669eff7bb99ea51f76c79e7fdbc46c17278f77
root <root@LAPTOP-7VB8MDDP.localdomain>
2017-12-16 16:15:36 +0530

    added new part to ignore the effects of presence of subdirectories. Exit code still left

--- countFiles.sh
+++ countFiles.sh
@@ -2,14 +2,28 @@
 initial=`pwd`
 cd $1
 count="0"
-x=`ls *$2`
+x=`ls *$2 2>/dev/null`
 if [ "$?" -eq "0" ]
- then
- for i in *$2
-  do 
+then
+ for i in $x
+  do
   count=`expr $count \+ 1`
-  done
+ done
+
 fi
 
+if [ -z "$2" ]
+then
+
+x=`ls -d */ 2>/dev/null`
+ if [ "$?" -eq "0" ]
+ then
+   for i in $x
+     do
+    count=`expr $count \- 1`
+    done
+ fi
+
+fi
 echo $count
 cd $initial
